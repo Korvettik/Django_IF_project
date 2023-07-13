b0fb17b408532ce4a67f83cd7297643cf942034b
Korvettik <korvet1988@yandex.ru>
2023-07-13 12:27:44 +0300

    ORM, созданы модели ProductCategory, Product в models.py

--- store-server/store/products/models.py
+++ store-server/store/products/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class ProductCategory(models.Model):
+    # id создается по умолчанию
+    name = models.CharField(max_length=128, unique=True)
+    description = models.TextField(null=True, blank=True)  # не обязательно к заполнению
+
+
+
+class Product(models.Model):
+    # id создается по умолчанию
+    name = models.CharField(max_length=256)
+    description = models.TextField()   # обязательно к заполнению
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=0)
+    image = models.ImageField(upload_to='products_images')
+    category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
