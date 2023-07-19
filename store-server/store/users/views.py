81035c5117472a0ffa1c1ea2fe534b9beacef6c6
Korvettik <korvet1988@yandex.ru>
2023-07-19 16:42:33 +0300

    контроллер авторизации. Добавление POST логики от формы во views.py и login.html

--- store-server/store/users/views.py
+++ store-server/store/users/views.py
@@ -1,10 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib import auth
+from django.urls import reverse
 
 from users.models import User
 from users.forms import UserLoginForm
 
 def login(request):
-    context = {'form': UserLoginForm()}  # передаем по ключу и вызываем
+    if request.method == 'POST':  # POST
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+    else:  # GET
+        form = UserLoginForm()
+    context = {'form': form}  # передаем по ключу и вызываем
     return render(request, 'users/login.html', context)
 
 
