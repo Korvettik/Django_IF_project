970c148f94c9f8dd74525fe70a6c7d002074ddca
Korvettik <korvet1988@yandex.ru>
2023-07-20 11:28:01 +0300

    Собственный шаблон и кастомизация формы. Корректировка forms.py  login.html

--- store-server/store/users/forms.py
+++ store-server/store/users/forms.py
@@ -1,7 +1,14 @@
+from django import  forms
 from django.contrib.auth.forms import AuthenticationForm
 from users.models import User
 
 class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4',
+        'placeholder': 'Введите имя пользователя'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control py-4',
+        'placeholder': 'Введите пароль'}))
     class Meta:
         model = User  # указываем модель-строку из бд
         fields = ('username', 'password')  # какие поля берем для обработки, кортеж, можно список
