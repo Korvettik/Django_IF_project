7862f9091486468aac70770a730db0fddea06f0b
Korvettik <korvet1988@yandex.ru>
2023-07-25 12:52:59 +0300

    прописан контроллер logout, админ панель только для суперпользователя

--- store-server/store/users/urls.py
+++ store-server/store/users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from users.views import login, registration, profile  #ничего страшного, что подсвечивает
+from users.views import login, registration, profile, logout  #ничего страшного, что подсвечивает
 
 app_name = 'users'
 
 urlpatterns = [
     path('login/', login, name='login'),  # ../users/login
-    path('registration', registration, name='registration'),  # ../users/registration
-    path('profile', profile, name='profile'),  # ../users/profile
+    path('registration/', registration, name='registration'),  # ../users/registration
+    path('profile/', profile, name='profile'),  # ../users/profile
+    path('logout/', logout, name='logout'),  # ../users/logout
 ]
