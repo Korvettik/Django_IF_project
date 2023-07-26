a9f50205dd70f6e55c601f56301f73744f51cf5c
Korvettik <korvet1988@yandex.ru>
2023-07-26 13:48:32 +0300

    сделана логика корзины, добавлены обработчики событий для нее

--- store-server/store/products/models.py
+++ store-server/store/products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from users.models import User
 
 # Create your models here.
 class ProductCategory(models.Model):
@@ -22,3 +23,14 @@ class Product(models.Model):
 
     def __str__(self):
         return f'Продукт: {self.name}, Категория: {self.category.name}'
+
+
+class Basket(models.Model):
+    # id создается по умолчанию
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    product = models.ForeignKey(to=Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=0)
+    created_timestamp = models.DateTimeField(auto_now_add=True)  # автоматически заполняется
+
+    def __str__(self):
+        return f'Корзина для {self.user.username} | Продукт: {self.product.name}'
